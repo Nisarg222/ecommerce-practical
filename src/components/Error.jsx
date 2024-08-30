c7d9df48f544a5d05c89a7e1424d542b58b2b850
nisarg soni <nisargsoni2410@gmail.com>
2024-08-30 15:07:49 +0530

    changes

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,7 +7,7 @@ const Error = () => {
         <div>
             <Back title='404 Error'/>
                 <div className="error m-auto text-center">
-                    <span className='text-7xl'><i class="fa fa-exclamation-triangle" style={{color: '#ffb524'}}></i></span>
+                    <span className='text-7xl'><i className="fa fa-exclamation-triangle" style={{color: '#ffb524'}}></i></span>
                     <h2 className='font-bold text-7xl my-6' style={{color: '#45595b'}}>404</h2>
                     <h1 className='font-semibold text-4xl' style={{color: '#45595b'}}>Page Not Found</h1>
                     <p className='my-6 w-8/12 m-auto' style={{color: '#45595b'}}>We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
